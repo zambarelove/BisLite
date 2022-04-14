5fafbb766341e08a01df548d7145770c1483011d
zambarelove <zambarelove@gmail.com>
2022-04-14 16:41:59 +0530

    created Portfolio page

--- asstes/js/index.js
+++ asstes/js/index.js
@@ -118,3 +118,25 @@ function openNav() {
             }
         ]
     });
+
+    const tabs  = document.querySelectorAll('[data-tab-target]')
+    const tabContents = document.querySelectorAll('[data-tab-content]')
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click' , () => {
+            const target = document.querySelector(tab.dataset.tabTarget)
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('active')
+            }) 
+            target.classList.add('active')
+        })
+    })
+
+
+
+
+
+
+
+    
+        
