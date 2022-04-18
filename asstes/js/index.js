ebf93326eeb4621386a91e2abe98af496f79ca34
zambarelove <zambarelove@gmail.com>
2022-04-18 08:46:51 +0530

    Comlpeted website

--- asstes/js/index.js
+++ asstes/js/index.js
@@ -137,6 +137,12 @@ tabs.forEach(tab => {
   })
 })
 
+var waypoint = new Waypoint({
+    element: document.getElementById('waypoint'),
+    handler: function(direction) {
+      console.log('Scrolled to waypoint!')
+    }
+  })
 
 
 
